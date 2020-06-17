5df65e57254b333797f47d5f67e060cafa2a0c1d
Ali Imran <35903658+aliimran-pk@users.noreply.github.com>
2020-06-17 11:43:34 -0400

    yarn build

---
+++ build/precache-manifest.3f8f1f85a113fa59e151333b97b0c84d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce57afb415ab2508d59d07d093f7c3bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78172c11d314f763c277",
+    "url": "/static/css/main.6aa3799d.chunk.css"
+  },
+  {
+    "revision": "02d65ad6ec437f72acf1",
+    "url": "/static/js/2.070f1458.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.070f1458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78172c11d314f763c277",
+    "url": "/static/js/main.40e96f22.chunk.js"
+  },
+  {
+    "revision": "3e5e20c52f8e9bacf785",
+    "url": "/static/js/runtime-main.b6a55b0e.js"
+  }
+]);
